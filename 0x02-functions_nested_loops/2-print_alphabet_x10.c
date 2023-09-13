342b1b73b789f087e6390e325893a9adb1956e91
Grace-Mugo <mugograce21@gmail.com>
2023-09-13 08:54:27 -0700

    Print the alphabet in lowercase 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,13 @@
  *
  */
 
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	i=0;
-	while (i<10)
+	i = 0;
+
+	while (i < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
