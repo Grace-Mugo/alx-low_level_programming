207a4d5a403d1463276dc883dcdf14090a7b4b8c
Grace-Mugo <mugograce21@gmail.com>
2023-09-14 10:23:15 -0700

    Print 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints 0 to 9 excluding 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i != 2) && (i != 4))
+		{
+			_putchar('0' + i);
+		}
+		
+	}
+		_putchar('\n');
+}
