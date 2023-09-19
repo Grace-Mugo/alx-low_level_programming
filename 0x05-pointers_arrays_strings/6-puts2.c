b02d783beed4314a8a4389744138bd608566602a
Grace-Mugo <mugograce21@gmail.com>
2023-09-19 13:02:08 -0700

    Print every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Print one char out of 2 of a string
+ *
+ * @str: Type char pointer
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
+}
